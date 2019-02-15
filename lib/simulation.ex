667da69b7bb0b78013aace84d64952e776767825
Pushkin <abbott.pushkin@gmail.com>
2019-02-14 18:23:03 -0800

    boilerplate code

---
+++ lib/simulation.ex
@@ -0,0 +1,18 @@
+defmodule Simulation do
+  @moduledoc """
+  Documentation for Simulation.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Simulation.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
